819b3ce0f161f78115a568f50bd86b72269db908
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-30 11:17:21 +0100

    implement UserService

---
+++ NetlientHomework/Services/UserService.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NetlientHomework.Entities;
+using NetlientHomework.Entities.Models;
+using NetlientHomework.Entities.Models.DTOs;
+using NetlientHomework.Exceptions;
+
+namespace NetlientHomework.Services;
+
+public class UserService
+{
+    private readonly NetlientHomeworkContext _context;
+
+    public UserService(NetlientHomeworkContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserModel?> GetUserByName(string name)
+    {
+        return await _context.UserModel.FirstOrDefaultAsync(user => user.UserName == name);
+    }
+
+    public async Task<UserModel> AddNewUser(SignUpDTO user)
+    {
+        UserModel? foundUser = await GetUserByName(user.UserName);
+        if (foundUser != null)
+        {
+            throw new UserNameAlreadyInUseException("This user name is already in taken!");
+        }
+
+        var userToAdd = new UserModel
+        {
+            UserName = user.UserName,
+            Password = user.Password
+        };
+
+        _context.UserModel.Add(userToAdd);
+        await _context.SaveChangesAsync();
+        return userToAdd;
+    }
+}
