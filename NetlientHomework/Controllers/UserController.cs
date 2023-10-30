5e4ad829cf40147965650becddcf4b2f2c0b53d2
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-30 11:29:39 +0100

    create UserController

---
+++ NetlientHomework/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using NetlientHomework.Entities.Models;
+using NetlientHomework.Entities.Models.DTOs;
+using NetlientHomework.Services;
+
+namespace NetlientHomework.Controllers;
+
+[Route("/api/v1/users")]
+
+public class UserController : ControllerBase
+{
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+    
+    public async Task<UserModel> PostNewUser([FromBody] SignUpDTO user)
+    {
+        return await _userService.AddNewUser(user);
+
+    }
+}
