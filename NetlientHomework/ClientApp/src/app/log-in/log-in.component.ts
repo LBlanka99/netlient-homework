133d17e3a47aee380c4aaaf19ec0a14acca38fd1
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-02 10:55:28 +0100

    send login credentials to backend & handle errors

--- NetlientHomework/ClientApp/src/app/log-in/log-in.component.ts
+++ NetlientHomework/ClientApp/src/app/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
 import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: "app-log-in",
@@ -8,9 +11,34 @@ export class LogInComponent {
   username: string = "";
   password: string = "";
   errorMessage: string = "";
+  sub!: Subscription;
 
+  constructor(private httpClient: HttpClient, private router: Router) {}
   login() {
-    console.log('Username: ' + this.username);
-    console.log('Password: ' + this.password);
+    this.errorMessage = "";
+    // verify data
+    if (!this.username || !this.password) {
+      this.errorMessage = "Please fill in all required fields.";
+      return;
+    }
+
+    // send data to server
+    const apiAddress = "https://localhost:7181/api/v1/users/log-in";
+
+    const body = {
+      UserName: this.username,
+      Password: this.password,
+    };
+
+    this.sub = this.httpClient.post(apiAddress, body, {withCredentials: true}).subscribe({
+      next: res => console.log(res),
+      error: err => this.errorMessage = err.error
+    });
+
+
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
