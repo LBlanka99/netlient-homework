0bee81f4f500505ab2221e4b2cefde9cd6cdfdc3
LBlanka99 <losonci.blanka@mensa.hu>
2023-11-05 22:37:57 +0100

    delete unnecessary components

--- NetlientHomework/ClientApp/src/app/app.module.ts
+++ NetlientHomework/ClientApp/src/app/app.module.ts
@@ -7,11 +7,8 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import {LogInComponent} from "./log-in/log-in.component";
-import {ProductsComponent} from "./products/products.component";
 import {ProductModule} from "./products/product.module";
 
 @NgModule({
@@ -19,8 +16,6 @@ import {ProductModule} from "./products/product.module";
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     SignUpComponent,
     LogInComponent
   ],
@@ -30,8 +25,6 @@ import {ProductModule} from "./products/product.module";
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: "sign-up", component: SignUpComponent },
       { path: "log-in", component: LogInComponent }
     ]),
